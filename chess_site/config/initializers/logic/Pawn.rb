86c64f2058d54afb2b1625266c2f9fcfe6b8f8cf
Mithul <mithulmathi@gmail.com>
2016-04-18 11:39:10 +0530

    bug fix

--- chess_site/config/initializers/logic/Pawn.rb
+++ chess_site/config/initializers/logic/Pawn.rb
@@ -20,7 +20,7 @@ class Pawn < Piece
 					@possible_moves << move
 				end
 
-				if !@moved and @board[@x+2*advance][@y].piece == nil
+				if !@moved and ((@x==2 and advance==1) or (@x==7 and advance==-1)) and @board[@x+2*advance][@y].piece == nil
 					move = [@x+2*advance,@y,2]
 					@possible_moves << move
 					@moved = true
